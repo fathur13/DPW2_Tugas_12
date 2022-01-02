18871eedf48dc2554af98927b8c92dabd6e372d5
fikrizulfikarb <fathur34rahman56@gmail.com>
2022-01-02 13:05:24 +0700

    Initial commit

---
+++ system/app/Models/produk.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Models;
+
+use App\Models\User;
+use App\Models\Traits\Attributes\ProdukAttributes;
+use App\Models\Traits\Relations\ProdukRelations;
+
+class produk extends Model {
+	use ProdukAttributes,ProdukRelations;
+
+	protected $table = 'produk';
+
+	protected $casts = [
+		'created_at' => 'datetime',
+		'updated_at' => 'datetime',
+		'berat' => 'decimal:2'
+
+	];
+}
